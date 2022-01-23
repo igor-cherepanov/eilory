ceb357cea743f931d7a8691238c8555291178775
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2022-01-23 23:24:14 +0500

    feat();

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import {createStore} from 'vuex'
+import userModule from "./userModule";
 
 export default createStore({
     state: {},
     getters: {},
     mutations: {},
     actions: {},
-    modules: {}
+    modules: {
+        user: userModule
+    }
 })
--- src/store/userModule.js
+++ src/store/userModule.js
@@ -1,9 +1,24 @@
 import {createStore} from 'vuex'
 
 export default createStore({
-    state: {},
+    state: () => ({
+        id: {
+            type: String,
+            default: null,
+        },
+        token: {
+            type: String,
+            default: null,
+        },
+        isAuth: {
+            default: false,
+            type: Boolean
+        }
+    }),
     getters: {},
-    mutations: {},
+    mutations: {
+
+    },
     actions: {},
     modules: {}
 })
